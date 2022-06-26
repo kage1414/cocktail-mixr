c6cd3d305fe317761fa2963239894980ceb1e501
kage1414 <kylejohnson92294@gmail.com>
2022-06-26 18:16:36 -0400

    Init

---
+++ web/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'public.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: ['babel-loader', 'source-map-loader']
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'client'),
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ]
+
+};
